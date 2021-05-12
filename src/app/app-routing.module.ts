38708cec1c7bfbacc3f9651e5ab143b61676d2a3
Hemangi <hemangimalgaonkar98@hotmail.com>
2021-05-12 14:14:40 +0530

    admin navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddCategoryProductComponent } from './add-category-product/add-category-product.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
   {
-    path: "", component:AddCategoryProductComponent
+    path: "", component:LoginComponent
+  },
+  {
+    path: "addProductCategory", component:AddCategoryProductComponent
+  },
+  {
+    path: "navbar", component:NavbarComponent
+  },
+  {
+    path: "home", component:HomeComponent
   }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCategoryProductComponent } from './add-category-product/add-category-product.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddCategoryProductComponent
+    AddCategoryProductComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
